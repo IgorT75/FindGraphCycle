4c0a66b1d85465f05e8ea6a51d01fb3c8a0d103f
IgorT75 <igort75@yandex.ru>
2020-10-13 18:58:59 -0700

    Code file

--- FindGraphCycle.cpp
+++ FindGraphCycle.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <queue>
+#include <utility>
 #include <vector>
 #include <map>
 #include <stack>
@@ -14,106 +15,69 @@ void printVec(const vector<size_t>& v) {
   printf("\n");
 }
 
-vector<size_t> topologicalSortKahn(const vector<vector<size_t>>& graph) {
-  // find initial vertices with outgoings only (i.e. no incomings)
-  map<size_t, size_t> insMap; // vertex to # of incomings map
-  for(size_t i = 0, n = graph.size(); i < n; ++i)
-    insMap[i] = 0;
+class CycleFinder {
 
-  for (size_t vi = 0, gSz = graph.size(); vi < gSz; ++vi) {
-    const auto& v = graph[vi];
-    for(size_t i = 0, n = v.size(); i < n; ++i) {
-      ++insMap[v[i]];
-    }
-  }
-  
-  // push to the queue
-  queue<size_t> q;
-  for (size_t i = 0, n = insMap.size(); i < n; ++i)
-    if (insMap[i] == 0) q.push(i);
+	enum State { white = 0, gray = 1, black = 2 };
 
-  vector<size_t> res;
-  while (!q.empty()) {
-    size_t currVert = q.front();
-    q.pop();
-    res.push_back(currVert);
-    for (size_t adjVert : graph[currVert]) {
-      if (--insMap[adjVert] == 0) {
-        q.push(adjVert);
-        insMap.erase(adjVert);
-      }
-    }
-  }
-
-  return res;
-}
-
-class TopoSortDfs {
   vector<vector<size_t>> _graph;
   mutable vector<bool> _visited;
-  mutable stack<size_t> _stack;
+	mutable map<size_t, State> _state;
+
+	bool dfs(size_t vertex) const {
+		if (vertex >= _graph.size())
+			throw bad_exception();
+
+		_state[vertex] = gray;
+		printf("Graying vertex: %zu\n", vertex);
+		for (size_t adjVert : _graph[vertex])
+		{
+			if (_state[adjVert] == gray)
+				return true;
+			if (_state[adjVert] == white)
+				if (dfs(adjVert))
+					return true;
+		}
+		_state[vertex] = black;
+		printf("Blackened vertex: %zu\n", vertex);
+		return false;
+	}
+
   public:
-    TopoSortDfs(const vector<vector<size_t>>& graph) : _graph(graph) { }
+		explicit CycleFinder(vector<vector<size_t>> graph) : _graph(std::move(graph)) { }
+		explicit CycleFinder(const vector<vector<size_t>>&& graph) : _graph(graph) { }
 
-    void dfs(size_t vertex) const {
-      if (vertex >= _graph.size())
-        throw bad_exception();
-      
-      _visited[vertex] = true;
-      for (size_t i = 0, n = _graph[vertex].size(); i < n; ++i) {
-        size_t adjVert = _graph[vertex][i];
-        if (!_visited[adjVert])
-          dfs(adjVert);
-      }
-      //printf("Pushing: %zu\n", vertex);
-      _stack.push(vertex);
-    }
-
-    vector<size_t> topologicalSortDfs() {
-      _visited= vector<bool>(_graph.size(), false);
-      _stack = stack<size_t>();
+    bool findCycle() const {
       for (size_t i = 0, n = _graph.size(); i < n; ++i) {
-        if (!_visited[i])
-          dfs(i);
+				if (_state[i] == white)
+					if (dfs(i))
+						return true;
       }
-
-      vector<size_t> v;
-      while (!_stack.empty()) {
-        v.push_back(_stack.top());
-        _stack.pop();
-      }
-      return v;
+			return false;
     }
 };
 
 int main() {
 /* 0→→→→1←←←←←←←←←←←3
-   ↑    ↓ ⬉      ⬈ ↓ ⬊
-   ↑    ↓   ⬉  ⬈   ↓   ⬊
+   ↑    ↓ ⬊      ⬈ ↓ ⬊
+   ↑    ↓   ⬊  ⬈   ↓   ⬊
    ↑    ↓     2     ↓    4
    ↑    ↓  ⬋        ↓  ⬈
    ↑    ↓⬋          ↓⬈
    7→→→→6→→→→→→→→→→→5*/
-  vector<vector<size_t>> graph {
+	const vector<vector<size_t>> graph {
     { 1 },
-    { 6 },
-    { 1, 3, 6 },
-    { 1, 4, 5 },
-    {  },
+    { 2, 6 },
+    { 3, 6 },
+    { 4, 5 },
+    { 0 },
     { 4 },
     { 5 },
     { 0, 6 },
   };
 
-  auto topoSort1 = topologicalSortKahn(graph);
-  printf("Kahn's algorithm:\n");
-  printVec(topoSort1);
-  printf("\n");
-
-  TopoSortDfs ts(graph);
-  auto topoSort2 = ts.topologicalSortDfs();
-  printf("DFS-based algorithm:\n");
-  printVec(topoSort2);
+  CycleFinder ts(graph);
+	const bool isCycle = ts.findCycle();
+	printf("Cycle present: %d\n", isCycle);
 
   return 0;
 }
